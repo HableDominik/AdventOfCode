a13a7afb65370739ac578273b25a745f2e1368b6
dominikhable <habledominik@gmail.com>
2024-11-11 21:02:43 +0100

    add day01 2017

---
+++ AoC2017/Days/Day01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2017.Days;
+
+public class Day01 : BaseDay
+{
+    private readonly string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{SolveCommon(1)}");
+
+    public override ValueTask<string> Solve_2() => new($"{SolveCommon(_input.Length / 2)}");
+
+    private int SolveCommon(int offset)
+    {
+        var input = _input + _input[..offset];
+        return input
+            .Zip(input.Skip(offset), (current, next) => current == next ? current - '0' : 0)
+            .Sum();
+    }
+
+    private int Solve1()
+    {
+        var input = _input + _input[0];
+        return input
+            .Zip(input.Skip(1), (current, next) => current == next ? current - '0' : 0)
+            .Sum();
+    }
+
+    private int Solve2()
+    {
+        var halfLen = _input.Length / 2;
+        var input = _input + _input[..halfLen];
+        return input
+            .Zip(input.Skip(halfLen), (current, next) => current == next ? current - '0' : 0)
+            .Sum();
+    }
+}
