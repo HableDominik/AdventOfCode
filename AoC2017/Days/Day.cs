825934ae67350855e67d8dba5b45de2e413cb78e
dominikhable <habledominik@gmail.com>
2024-11-15 14:48:49 +0100

    add day02 2027

---
+++ AoC2017/Days/Day.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2017.Days;
+
+public class Day : BaseDay
+{
+    private readonly string _input;
+
+    public Day()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Solve1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Solve2()}");
+
+    private int Solve1()
+    {
+        return 0;
+    }
+
+    private int Solve2()
+    {
+        return 0;
+    }
+}
---
+++ AoC2017/Days/Day02.cs
@@ -0,0 +1,43 @@
+﻿namespace AoC2017.Days;
+
+public class Day02 : BaseDay
+{
+    private readonly IEnumerable<IEnumerable<int>> _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllText(InputFilePath)
+            .Split('\n')
+            .Select(line => line
+                .Split(' ', '\t')
+                .Select(int.Parse)
+        );
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{SolveCommon(MinMaxDiff)}");
+
+    public override ValueTask<string> Solve_2() => new($"{SolveCommon(EvenlyDivisible)}");
+
+    private int SolveCommon(Func<IEnumerable<int>, int> func)
+        => _input
+            .Select(line => func(line))
+            .Sum();
+
+    private int MinMaxDiff(IEnumerable<int> values)
+        => values.Max() - values.Min();
+
+    private static int EvenlyDivisible(IEnumerable<int> values)
+        => (from a in values
+            from b in values
+            where a != b && b % a == 0
+            select b / a).FirstOrDefault();
+
+    private int Solve1() => _input
+        .Select(line => line.Max() - line.Min())
+        .Sum();
+
+    private int Solve2() => _input
+        .Select(line => EvenlyDivisible(line))
+        .Sum();
+
+}
