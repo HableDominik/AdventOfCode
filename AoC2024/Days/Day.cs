8e0de892fa433ef8b25a031149d41d61082f427f
dominikhable <habledominik@gmail.com>
2024-11-30 18:57:47 +0100

    prepare AoC 2024

---
+++ AoC2024/Days/Day.cs
@@ -0,0 +1,25 @@
+﻿namespace AoC2024.Days;
+
+public class Day : BaseDay
+{
+    private readonly string _input;
+
+    public Day()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Solve1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Solve2()}");
+
+    private int Solve1()
+    {
+        return 0;
+    }
+
+    private int Solve2()
+    {
+        return 0;
+    }
+}
