54fe68b2009d1e6619b44bf78749b04ff4a9eff9
dominikhable <habledominik@gmail.com>
2025-01-11 13:29:38 +0100

    add day01 2018

---
+++ AoC2018/Days/Day01.cs
@@ -0,0 +1,32 @@
+﻿namespace AoC2018.Days;
+
+public class Day01 : BaseDay
+{
+    private readonly IEnumerable<int> _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllLines(InputFilePath).Select(int.Parse);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Solve1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Solve2()}");
+
+    private int Solve1() => _input.Sum();
+
+    private int Solve2()
+    {
+        int frequency = 0;
+        var frequencies = new HashSet<int>() { frequency };
+
+        while (true)
+        {
+            foreach (var fq in _input)
+            {
+                frequency += fq;
+                if (!frequencies.Add(frequency)) return frequency;
+            }
+        }
+    }
+}
