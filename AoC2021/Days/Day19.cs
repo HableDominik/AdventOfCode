7d1afcc9160976056333f1a44ec9b5d1ae3a53ee
dominikhable <habledominik@gmail.com>
2025-01-11 11:45:07 +0100

    prepare all

---
+++ AoC2021/Days/Day19.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+
+namespace AoC2021.Days;
+
+public class Day19 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day19()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Solve1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Solve2()}");
+
+    private int Solve1() => 0;
+
+    private int Solve2() => 0;
+}
